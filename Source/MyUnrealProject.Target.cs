300b8ed7e85425b64b242fddcfb6d4befbe6d7e0
Ankit <ankitk@mayhem-studios.com>
2025-09-02 14:31:22 +0530

    Adding all changes after setting up input bindings and character meshes

--- Source/MyUnrealProject.Target.cs
+++ Source/MyUnrealProject.Target.cs
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
@@ -8,8 +8,8 @@ public class MyUnrealProjectTarget : TargetRules
 	public MyUnrealProjectTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-
-		ExtraModuleNames.AddRange( new string[] { "MyUnrealProject" } );
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("MyUnrealProject");
 	}
 }
--- Source/MyUnrealProjectEditor.Target.cs
+++ Source/MyUnrealProjectEditor.Target.cs
@@ -1,15 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class MyUnrealProjectEditorTarget : TargetRules
 {
-	public MyUnrealProjectEditorTarget(TargetInfo Target) : base(Target)
+	public MyUnrealProjectEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-
-		ExtraModuleNames.AddRange( new string[] { "MyUnrealProject" } );
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("MyUnrealProject");
 	}
 }
